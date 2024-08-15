1f5970ee5199704f92c200fde3ec04ac42a84f1a
Adam Miller-Kirk <adam_kirk_msc@hotmail.com>
2024-08-15 18:46:42 +0100

    feature enabled basic paths and login system

---
+++ src/actions/index.ts
@@ -0,0 +1,7 @@
+// "use server";
+
+export { signIn } from "./sign-in";
+export { signOut } from "./sign-out";
+export { createComment } from "./create-comment";
+export { createPost } from "./create-post";
+export { createTopic } from "./create-topic";
---
+++ src/components/headerAuth.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import {
+  NavbarItem,
+  Button,
+  Avatar,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@nextui-org/react";
+import { useSession } from "next-auth/react";
+import React from "react";
+import { signIn, signOut } from "@/actions";
+import { signOut as nextAuthSignOut } from "next-auth/react";
+
+export default function HeaderAuth() {
+  const session = useSession();
+  let authContent: React.ReactNode;
+
+  if (session.status === "loading") {
+    authContent = null;
+  } else if (session.data?.user) {
+    authContent = (
+      <Popover placement="left">
+        <PopoverTrigger>
+          <Avatar src={session.data.user.image || ""} />
+        </PopoverTrigger>
+        <PopoverContent>
+          <form
+            className="p-4"
+            action={async () => {
+              await signOut();
+              await nextAuthSignOut({ redirect: false });
+            }}
+          >
+            <Button type="submit" color="secondary" variant="bordered">
+              Sign Out
+            </Button>
+          </form>
+        </PopoverContent>
+      </Popover>
+    );
+  } else {
+    authContent = (
+      <>
+        <NavbarItem>
+          <form action={signIn}>
+            <Button type="submit" color="secondary" variant="bordered">
+              Sign In
+            </Button>
+          </form>
+        </NavbarItem>
+        <NavbarItem>
+          <form action={signIn}>
+            <Button type="submit" color="primary" variant="flat">
+              Sign Up
+            </Button>
+          </form>
+        </NavbarItem>
+      </>
+    );
+  }
+
+  return authContent;
+}
